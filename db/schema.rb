6b2ef1fec03e1ecf2e4ed07c7f1563c441a3e894
en-Barry <en_barry@icloud.com>
2021-02-19 00:36:11 +0900

    Add: トップページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_144016) do
+ActiveRecord::Schema.define(version: 2021_02_18_144857) do
 
   create_table "user", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", null: false
