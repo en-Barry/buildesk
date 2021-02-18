09824e9ab627384d13725b53371542247f2fb7cf
en-Barry <en_barry@icloud.com>
2021-02-18 19:53:20 +0900

    ingnoreの記載とキャッシュの削除

--- vendor/bundle/ruby/2.6.0/gems/redis-store-1.9.0/lib/redis/store/version.rb
+++
@@ -1,5 +0,0 @@
-class Redis
-  class Store < self
-    VERSION = '1.9.0'
-  end
-end
--- vendor/bundle/ruby/2.6.0/gems/slim_lint-0.20.2/lib/slim_lint/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Defines the gem version.
-module SlimLint
-  VERSION = '0.20.2'
-end
