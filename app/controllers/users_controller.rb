53c410a33b44811e466a3f06eb3f890bc16f3bd6
en-Barry <en_barry@icloud.com>
2021-02-21 01:35:38 +0900

    Add: ユーザー登録のRSpecを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'Register successful'
     else
+      flash.now[:alert] = 'Register failed'
       render :new
     end
   end
