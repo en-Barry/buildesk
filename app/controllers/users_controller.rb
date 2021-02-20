685c49e2d94477edead993b628ab791bfe896e19
en-Barry <en_barry@icloud.com>
2021-02-20 20:44:15 +0900

    Add: user_paramsにimageを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :image)
   end
 end
