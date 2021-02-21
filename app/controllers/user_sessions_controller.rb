537f80528cb4d7fcb797a8606e01d723f73cc7a8
en-Barry <en_barry@icloud.com>
2021-02-21 23:28:16 +0900

    Fix: 翻訳部分のキーを統一

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,15 +9,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to root_path, success: t('.Login successful')
+      redirect_back_or_to root_path, success: t('.success')
     else
-      flash.now[:danger] = t('.Login failed')
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, success: t('.Logged out')
+    redirect_to root_path, success: t('.success')
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path, success: t('.Register successful')
+      redirect_to root_path, success: t('.success')
     else
-      flash.now[:danger] = t('.Register failed')
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
