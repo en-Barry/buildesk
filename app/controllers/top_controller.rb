482c991583ddec0576e8a12d3f2ee7b7d114dd1a
en-Barry <en_barry@icloud.com>
2021-02-22 03:29:09 +0900

    Add: 投稿一覧取得と表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,6 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   
   def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 end
