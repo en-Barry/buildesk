0376fdfcf0667dd3c526e0ee1f85bb8454aa1e96
en-Barry <en_barry@icloud.com>
2021-02-22 20:17:56 +0900

    Add: アソシエーション の設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+  
   validates :name, presence: true, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_many :post_category_categories, through: :post_categories, source: :category
+  has_many :post_categories
+  has_many :categories, through: :post_categories
   belongs_to :user
 
   validates :body, length: { maximum: 65535 }
