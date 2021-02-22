4d5d6d6436827a27a9501a701a8d51da4c633fc5
en-Barry <en_barry@icloud.com>
2021-02-22 17:24:08 +0900

    Add: アソシエーションとバリデーション制約

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :post_category_categories, through: :post_categories, source: :category
   belongs_to :user
 
   validates :body, length: { maximum: 65535 }
--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,6 @@
 class PostCategory < ApplicationRecord
   belongs_to :category
   belongs_to :post
+
+  validates :post_id, uniqueness: { scope: category_id }
 end
