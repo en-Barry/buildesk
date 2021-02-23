32484389ed222c9e4796f1acdcf36f430d22328d
en-Barry <en_barry@icloud.com>
2021-02-23 23:20:49 +0900

    Add: タイムゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,10 @@ module Desktour
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    # タイムゾーン
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # i18nの設定
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
