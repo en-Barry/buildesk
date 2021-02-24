62d83ed7652ed776b94baee7eb2bd38c321690f8
en-Barry <en_barry@icloud.com>
2021-02-25 04:39:59 +0900

    Fix: 投稿取得クエリの変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,9 +2,9 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   
   def index
-    @posts = Post.all.includes(:user).order(created_at: :desc).limit(3)
-    @engineer_posts = Category.find_by(name: 'Engineer').post_categories.includes(:post).limit(3)
-    @writer_posts = Category.find_by(name: 'Writer').post_categories.includes(:post).limit(3)
-    @vc_posts = Category.find_by(name: 'Video creator').post_categories.includes(:post).limit(3)
+    @posts = Post.includes(:user, :categories).order(created_at: :desc).limit(3)
+    @engineer_posts = PostCategory.where(category_id: 1).includes(:post, :category).order(created_at: :desc).limit(3)
+    @writer_posts = PostCategory.where(category_id: 2).includes(:post, :category).order(created_at: :desc).limit(3)
+    @vc_posts = PostCategory.where(category_id: 3).includes(:post, :category).order(created_at: :desc).limit(3)
   end
 end
