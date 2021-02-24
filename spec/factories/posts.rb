72d821801ea7eaa6b44de379da26691c61d076e0
en-Barry <en_barry@icloud.com>
2021-02-24 23:06:32 +0900

    Fix: postの修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,4 +3,22 @@ FactoryBot.define do
     sequence(:body) { |n| "test_body_#{n}" }
     association :user
   end
+
+  trait :with_engineer do
+    after(:create) do |post|
+      create(:post_category, post: post, category: create(:category, :engineer))
+    end
+  end
+
+  trait :with_writer do
+    after(:create) do |post|
+      create(:post_category, post: post, category: create(:category, :writer))
+    end
+  end
+
+  trait :with_videocreator do
+    after(:create) do |post|
+      create(:post_category, post: post, category: create(:category, :video_creator))
+    end
+  end
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,23 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
   describe "トップページの表示" do
-    let!(:post1) { create(:post) }
-    let!(:post2) { create(:post) }
-    let!(:post3) { create(:post) }
-    let!(:category_engineer) { create(:category, :engineer) }
-    let!(:category_writer) { create(:category, :writer) }
-    let!(:category_videocreator) { create(:category, :video_creator) }
-    let!(:post_engineer) { create(:post_category, post: post1, category: category_engineer) }
-    let!(:post_writer) { create(:post_category, post: post2, category: category_writer) }
-    let!(:post_videocreator) { create(:post_category, post: post3, category: category_videocreator) }
+    let!(:post_with_engineer) { create(:post, :with_engineer) }
+    let!(:post_with_writer) { create(:post, :with_writer) }
+    let!(:post_with_videocreator) { create(:post, :with_videocreator) }
 
     context "新着の表示" do
       it '最新の投稿が表示される' do
         visit root_path
         within('.new_arrival') do
-          expect(page).to have_content(post_engineer.post.user.name)
-          expect(page).to have_content(post_writer.post.user.name)
-          expect(page).to have_content(post_videocreator.post.user.name)
+          expect(page).to have_content(post_with_engineer.user.name)
+          expect(page).to have_content(post_with_writer.user.name)
+          expect(page).to have_content(post_with_videocreator.user.name)
         end
       end
     end
@@ -26,22 +20,25 @@ RSpec.describe "Posts", type: :system do
       it "エンジニアの投稿が表示される" do
         visit root_path
         within('.post_engineer') do
-          expect(page).to have_content(post_engineer.post.user.name)
+          expect(page).to have_content(post_with_engineer.user.name)
         end
       end
       it "ライターの投稿が表示される" do
         visit root_path
         within('.post_writer') do
-          expect(page).to have_content(post_writer.post.user.name)
+          expect(page).to have_content(post_with_writer.user.name)
         end
       end
       it "動画系クリエイターの投稿が表示される" do
         visit root_path
         within('.post_videocreator') do
-          expect(page).to have_content(post_videocreator.post.user.name)
+          expect(page).to have_content(post_with_videocreator.user.name)
         end
       end
     end
+  end
+  describe "カテゴリ別一覧の表示" do
+    
   end
   
 end
