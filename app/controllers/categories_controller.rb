905f645ec6ca6d387f259efda1043a2b495c8e1d
en-Barry <en_barry@icloud.com>
2021-02-25 00:53:19 +0900

    Add: カテゴリーページのログインスキップ設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :require_login, only: %i[show]
 
   def show
     @posts = Category.find(params[:id]).post_categories.includes(:post).order(created_at: :desc)
