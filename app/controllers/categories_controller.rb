aa6635694a873dd64e2937208c54d6160e4cc66c
en-Barry <en_barry@icloud.com>
2021-02-25 03:45:33 +0900

    Add: set_categoryの追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,18 @@
 class CategoriesController < ApplicationController
   skip_before_action :require_login, only: %i[index show]
+  before_action :set_category, only: %i[show]
 
   def index 
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def show
-    @posts = Category.find(params[:id]).post_categories.includes(:post).order(created_at: :desc)
+    @posts = @category.post_categories.includes(:post).order(created_at: :desc)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
