7acb3d92182de82f9e42c6f04d3d870ae300de99
en-Barry <en_barry@icloud.com>
2021-02-27 05:57:19 +0900

    Fix: Postモデルのバリデーションテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context '全てのフィールドが有効な場合' do
-    let!(:post) { create(:post, :with_engineer ) }
     it '有効である' do
+      post = build(:post, :with_engineer)
       expect(post).to be_valid
     end
   end
   context 'bodyが65535字以内入力された場合' do
-    let!(:post) { create(:post, :with_engineer, body: 'a' * 65_535) }
     it '有効である' do
+      post = build(:post, :with_engineer, body: 'a' * 65_535)
       expect(post).to be_valid
     end
   end
