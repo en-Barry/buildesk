6650da91237f4d1c8b9deb732a9e097a15316407
en-Barry <en_barry@icloud.com>
2021-02-27 02:26:11 +0900

    Fix: 投稿取得時にユーザー情報を紐付け

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,9 +2,9 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   
   def index
-    @posts = Post.order(created_at: :desc).limit(3)
-    @engineer_posts = Category.find_by(name: 'Engineer').post_categories.includes(:post).order(created_at: :desc).limit(3)
-    @writer_posts = Category.find_by(name: 'Writer').post_categories.includes(:post).order(created_at: :desc).limit(3)
-    @creator_posts = Category.find_by(name: 'MediaCreator').post_categories.includes(:post).order(created_at: :desc).limit(3)
+    @posts = Post.includes(:user).order(created_at: :desc).limit(3)
+    @engineer_posts = Category.find_by(name: 'Engineer').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
+    @writer_posts = Category.find_by(name: 'Writer').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
+    @creator_posts = Category.find_by(name: 'MediaCreator').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
   end
 end
