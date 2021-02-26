dfeb456f448bcc0644850fbfb07792f295690063
en-Barry <en_barry@icloud.com>
2021-02-27 02:39:21 +0900

    Add: 投稿機能の変数定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,34 @@
 class PostsController < ApplicationController
+
+  def index 
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+
   def new
     @post = Post.new
   end
 
   def create 
-    @post = Post.new(post_param)
+    @post = current_path.posts.build(post_param)
+    if @post.save
+      redirect_to categories_path(@post.category.name), success: t('')
+    else
+      flash.now['danger'] = t('')
+      render :new
+    end
+  end
+
+  def update 
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+
   end
 end
