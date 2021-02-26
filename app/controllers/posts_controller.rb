e4ceb53b9f5c68e0cad8cb4d2a25f7b4ea9c6540
en-Barry <en_barry@icloud.com>
2021-02-27 04:12:36 +0900

    Fix: 投稿作成時のリダイレクト先

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   def create 
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to categories_path(@post.category.name), success: t('defaults.message.created', item: Post.model_name_human)
+      redirect_to categories_path, success: t('defaults.message.created', item: Post.model_name_human)
     else
       flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name_human)
       render :new
