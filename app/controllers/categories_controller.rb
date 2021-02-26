8db26ff5a12178727d7d7756551e4b0277ea229d
en-Barry <en_barry@icloud.com>
2021-02-27 02:20:44 +0900

    Fix: 投稿取得時にユーザー情報を紐付け

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by(name: params[:name])
-    @posts = @category.post_categories.includes(:post).order(created_at: :desc)
+    @posts = @category.post_categories.includes(post: :user).order(created_at: :desc)
   end
 
   private
