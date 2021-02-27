bfcc6d6c38c4dc2e47c30d39166c121877134e36
en-Barry <en_barry@icloud.com>
2021-02-28 05:43:13 +0900

    Fix: 投稿写真のurl指定メソッド

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,6 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    
     if @post.save
       redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
     else
@@ -27,6 +26,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:body, { post_images_attributes: [:image, :caption]}, { category_ids: [] })
+    params.require(:post).permit(:body, post_images_attributes: [:image, :caption], category_ids: [])
   end
 end
