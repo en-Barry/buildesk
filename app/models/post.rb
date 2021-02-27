21cda6e4d1d9dfe53037936a596fa1610e8f0e24
en-Barry <en_barry@icloud.com>
2021-02-28 04:11:20 +0900

    Add: アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :post_categories
   has_many :categories, through: :post_categories
+  has_many :post_images, dependent: :destroy
   belongs_to :user
 
   validates :body, length: { maximum: 65_535 }
