5a2603e61ce30fb84f09d5f714591509e7866e14
en-Barry <en_barry@icloud.com>
2021-02-28 04:27:10 +0900

    Add: 画像サイズmediumの設定

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -29,9 +29,9 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :medium do
+    process resize_to_fit: [1080, 1080]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
