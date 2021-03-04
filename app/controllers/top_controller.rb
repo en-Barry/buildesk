51fe667ab5907105049482b3edccdd1da52f5ef2
en-Barry <en_barry@icloud.com>
2021-03-05 01:34:50 +0900

    Fix: n+1が発生していたため取得式を変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,8 @@ class TopController < ApplicationController
 
   def index
     @posts = Post.includes(:user, :post_images).order(created_at: :desc).limit(3)
-    @engineer_posts = Category.find_by(name: 'Engineer').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
-    @writer_posts = Category.find_by(name: 'Writer').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
-    @creator_posts = Category.find_by(name: 'MediaCreator').post_categories.includes(post: :user).order(created_at: :desc).limit(3)
+    @engineer_posts = Category.find_by(name: 'Engineer').post_categories.includes(post: :user).includes(post: :post_images).order(created_at: :desc).limit(3)
+    @writer_posts = Category.find_by(name: 'Writer').post_categories.includes(post: :user).includes(post: :post_images).order(created_at: :desc).limit(3)
+    @creator_posts = Category.find_by(name: 'MediaCreator').post_categories.includes(post: :user).includes(post: :post_images).order(created_at: :desc).limit(3)
   end
 end
