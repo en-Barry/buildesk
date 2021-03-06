994bcc0d07f133d9cd7c489c59d6b3863b2dd64e
en-Barry <en_barry@icloud.com>
2021-03-07 04:21:41 +0900

    Add: コメント機能のテストを追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -19,9 +19,9 @@ RSpec.describe "Comments", type: :system do
     describe "コメントの一覧" do
       context "投稿詳細ページにアクセス" do
         it "投稿に紐づくコメントが表示される" do
-          within '#js-post-comment'do
-            expect(page).to have_cantent(comment_by_me.body)
-            expect(page).to have_cantent(comment_by_another.user.name)  
+          within('#js-post-comment') do
+            expect(page).to have_content(comment_by_me.body)
+            expect(page).to have_content(comment_by_another.user.name)  
           end
         end
       end
@@ -30,13 +30,21 @@ RSpec.describe "Comments", type: :system do
     describe "コメントの作成" do
       context "コメントを入力して作成" do
         it "作成に成功する" do
-          
+          fill_in 'コメント', with: 'test'
+          click_on '投稿'
+          comment = Comment.last
+          within('#js-post-comment') do
+            expect(page).to have_content(comment.user.name)
+            expect(page).to have_content(comment.body)
+          end
         end
       end
 
       context "コメントを未入力で作成" do
         it "作成に失敗する" do
-
+          fill_in 'コメント', with: ''
+          click_on '投稿'
+          expect(page).to have_content('コメントを作成できませんでした') 
         end
       end
     end
@@ -44,11 +52,12 @@ RSpec.describe "Comments", type: :system do
     describe "コメントの編集" do
       context "他人のコメントの場合" do
         it "編集ボタンと削除ボタンが表示されない" do
-          
+          within("#comment-#{comment_by_another.id}") do
+            expect(page).not_to have_selector('.js-edit-comment-button')
+            expect(page).not_to have_selector('.js-delete-comment-button')
+          end
         end
       end
     end
-    
   end
-  
 end
