509b312da423cd3db2db7a2794175b0d5a7f508f
en-Barry <en_barry@icloud.com>
2021-03-10 04:31:46 +0900

    Remove: 不必要な?の削除

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Comment, type: :model do
   context "全てのフィールドが有効な場合" do
     it '有効である' do
       comment = build(:comment)
-      expect(comment).to be_valid?
+      expect(comment).to be_valid
     end
   end
   context "bodyが未入力の場合" do
     it '無効である' do
       comment = build(:comment, body: nil)
-      expect(comment).to be_invalid?
+      expect(comment).to be_invalid
     end
   end
 end
