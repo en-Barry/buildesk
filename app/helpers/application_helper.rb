503b68cdadec20b709a90762d93a0d8be1fc8c0f
en-Barry <en_barry@icloud.com>
2021-03-15 22:30:06 +0900

    Fix: タイポのため修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def page_title(title = '')
+  def page_title(page_title = '')
     base_title = 'desktour'
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
