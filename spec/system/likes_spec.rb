fd94254163f29a7bf6b74226e615b8f0728b7d23
en-Barry <en_barry@icloud.com>
2021-03-22 18:48:31 +0900

    Add: いいね機能に関するテストの追加

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :system do
+  let(:user) { create(:user) }
+  let!(:post_by_uesr) { create(:post, user: user) }
+  let!(:post_by_others) { create(:post) }
+
   describe "いいね機能" do
-    let(:user) { create(:user) }
-    let!(:post_by_uesr) { create(:post, user: user) }
-    let!(:post_by_others) { create(:post) }
-    context "under condition" do
-      it "behaves like" do
-        
+    context "他人の投稿の場合" do
+      it "いいねボタンが表示される" do
+        login(user)
+        visit post_path(post_by_others)
+        expect(page).to have_selector("#button-good-#{post_by_others.id}")  
       end
     end
+    context "自分の投稿の場合" do
+      it "いいねボタンが表示されない" do
+        login(user)
+        visit post_path(post_by_uesr)
+        expect(page).not_to have_selector("#button-good-#{post_by_uesr.id}")  
+      end
+    end
+    
   end
   
 end
