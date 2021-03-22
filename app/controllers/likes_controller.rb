428bf756d4766c55be0a8ec0713f7fc870852e98
en-Barry <en_barry@icloud.com>
2021-03-22 20:35:56 +0900

    Add: いいね機能のアクションを実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
+    @like = Like.new(user_id: current_user.id, post_id: params[:id])
+    @like.save!
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @like = Like.find_by(post_id: params[:id])
+    @like.destroy!
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
