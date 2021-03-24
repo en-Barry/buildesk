c3bac277902a5282050521663d831b630c49b9f9
en-Barry <en_barry@icloud.com>
2021-03-24 19:51:41 +0900

    Fix: Lint

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :user_id, uniqueness: { scope: :post_id } 
+  validates :user_id, uniqueness: { scope: :post_id }
 end
