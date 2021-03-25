45c07b5a96f496cfc4f214b6b4966ae848ae8cf1
en-Barry <en_barry@icloud.com>
2021-03-25 23:27:26 +0900

    Add: ユーザー登録と同時にログインするための処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      login(params[:user][:email], params[:user][:password])
       redirect_to root_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
