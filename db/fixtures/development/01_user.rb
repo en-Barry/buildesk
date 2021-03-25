7ef2d825668f1c7a192514aa13be033562e6d528
en-Barry <en_barry@icloud.com>
2021-03-25 21:40:17 +0900

    Fix: syntaxエラーの解消

--- db/fixtures/development/01_user.rb
+++ db/fixtures/development/01_user.rb
@@ -1,8 +1,8 @@
 10.times do
   User.seed do |s|
-    s.name: Faker::Name.name,
-    s.email: Faker::Internet.email,
-    s.password: 'password',
-    s.password_confirmation: 'password'
+    s.name = Faker::Name.name,
+    s.email = Faker::Internet.email,
+    s.password = 'password',
+    s.password_confirmation = 'password'
   end
 end
--- db/fixtures/development/03_post.rb
+++ db/fixtures/development/03_post.rb
@@ -1,7 +1,7 @@
 5.times do
   Post.seed do |s|
-    s.body: "test_#{indes}",
-    s.area: 0,
-    s.user_id: User.offset(rand(User.count)).first.id
+    s.body = "test_#{s}",
+    s.area = 0,
+    s.user_id = User.offset(rand(User.count)).first.id
   end
 end
