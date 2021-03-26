fbd5857a1cdae98d7e879e5d6d8f0f0d8662f041
en-Barry <en_barry@icloud.com>
2021-03-27 00:01:40 +0900

    Add: プロフィール編集に関するテストの追加とマイページ表示のテストを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe 'Users', type: :system do
           fill_in 'パスワード',	with: 'password'
           fill_in 'パスワード確認',	with: 'password'
           click_button '登録する'
-          expect(current_path).to eq root_path
-          expect(page).to have_content('ユーザー登録しました')
+          expect(current_path).to eq root_path, 'ページが正しく遷移されていません'
+          expect(page).to have_content('ユーザー登録しました'), 'サクセスメッセージが表示されていません'
         end
       end
       context 'メールアドレスが未入力' do
@@ -31,9 +31,9 @@ RSpec.describe 'Users', type: :system do
           fill_in 'パスワード',	with: 'password'
           fill_in 'パスワード確認',	with: 'password'
           click_button '登録する'
-          expect(current_path).to eq users_path
-          expect(page).to have_content('ユーザー登録に失敗しました')
-          expect(page).to have_content('メールアドレスを入力してください')
+          expect(current_path).to eq users_path, 'ページが正しく遷移されていません'
+          expect(page).to have_content('ユーザー登録に失敗しました'), 'エラーメッセージが表示されていません'
+          expect(page).to have_content('メールアドレスを入力してください'), 'フォームのエラーが表示されていません'
         end
       end
       context '登録済のメールアドレスを入力' do
@@ -44,23 +44,61 @@ RSpec.describe 'Users', type: :system do
           fill_in 'パスワード',	with: 'password'
           fill_in 'パスワード確認',	with: 'password'
           click_button '登録する'
-          expect(current_path).to eq users_path
-          expect(page).to have_content('ユーザー登録に失敗しました')
-          expect(page).to have_content('メールアドレスはすでに存在します')
+          expect(current_path).to eq users_path, 'ページが正しく遷移されていません'
+          expect(page).to have_content('ユーザー登録に失敗しました'), 'エラーメッセージが表示されていません'
+          expect(page).to have_content('メールアドレスはすでに存在します'), 'フォームのエラーが表示されていません'
         end
       end
     end
   end
 
   describe "ログイン後" do
-    context "マイページの表示" do
-      it "ページが正しく表示される" do
-        login(user)
-        visit user_path(user)
-        expect(page).to have_selector("user-id-#{user.id}"), 'ユーザー情報が表示されていません'
-        expect(page).not_to have_selector("user-id-#{user_others.id}"), '別のユーザー情報が表示されています'
-        expect(page).to have_selector("post-id-#{post.id}"), 'ユーザーの投稿が表示されていません'
-        expect(page).not_to have_selector("post-id-#{post_by_others.id}"), '他人の投稿が表示されています'  
+
+    before do
+      login(user)
+    end
+
+    describe "マイページの表示" do
+      content "自分のマイページを表示" do
+        it "正しく表示される" do
+          visit user_path(user)
+          expect(page).to have_selector("user-id-#{user.id}"), 'ユーザー情報が表示されていません'
+          expect(page).not_to have_selector("user-id-#{user_others.id}"), '別のユーザー情報が表示されています'
+          expect(page).to have_selector("post-id-#{post.id}"), 'ユーザーの投稿が表示されていません'
+          expect(page).not_to have_selector("post-id-#{post_by_others.id}"), '他人の投稿が表示されています'  
+        end
+      end
+      content "他人のマイページを表示" do
+        it "正しく表示される" do
+          visit
+          expect(page).not_to have_selector("user-id-#{user.id}"), '自分のユーザー情報が表示されています'
+          expect(page).to have_selector("user-id-#{user_others.id}"), 'ユーザー情報が表示されていません'
+          expect(page).not_to have_selector("post-id-#{post.id}"), '自分の投稿が表示されています'
+          expect(page).to have_selector("post-id-#{post_by_others.id}"), 'ユーザーの投稿が表示されていません'
+        end
+      end
+    end
+
+    describe "プロフィールの編集" do
+      context "フォームの入力値が正常" do
+        it "更新に成功する" do
+          visit edit_profile_path(user)
+          fill_in 'ユーザーネーム', with: 'username'
+          fill_in '自己紹介', with: 'description_test'
+          attach_file 'プロフィール画像', "#{Rails.root}/spec/fixtures/20210227_005224.jpg"
+          click_on '保存する'
+          expect(current_path).to eq(), 'ページが正しく遷移していません'
+          expect(page).to have_content('プロフィールの編集に成功しました'), 'サクセスメッセージが表示されていません'  
+        end
+      end
+      context "ユーザーネームが未入力" do
+        it "更新に失敗する" do
+          visit edit_profile_path(user)
+          fill_in 'ユーザーネーム', with: nil
+          click_on '保存する'
+          expect(current_path).to eq(), 'ページが正しく遷移していません'
+          expect(page).to have_content('プロフィールの編集に失敗しました'), 'エラーメッセージが表示されていません'
+        end
       end
     end
   end
