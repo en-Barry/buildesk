33b417bce3e658c3ee0cdcb2fb61a79ef254843f
en-Barry <en_barry@icloud.com>
2021-03-26 15:42:26 +0900

    Add: ユーザー情報とユーザーに紐づく投稿を取得するための処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @post = current_user.posts.includes(:user).order(created_at: :desc)
+  end
+
   def create
     @user = User.new(user_params)
 
