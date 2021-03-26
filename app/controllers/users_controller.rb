3e4ed3a9f70833db31191007547c9720e8499196
en-Barry <en_barry@icloud.com>
2021-03-26 15:45:55 +0900

    Fix: 複数投稿を明示的にするためにインスタンス名を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post = current_user.posts.includes(:user).order(created_at: :desc)
+    @posts = current_user.posts.includes(:user).order(created_at: :desc)
   end
 
   def create
