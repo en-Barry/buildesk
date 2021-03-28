40bcb70cb5dab7fdee8107335f2f8b70d1fce425
en-Barry <en_barry@icloud.com>
2021-03-28 21:45:34 +0900

    Add: 統一したサイズでアイコン写真を登録するためのバージョンを指定

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -30,9 +30,9 @@ class UserImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
