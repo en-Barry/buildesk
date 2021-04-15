dd77e9aaabb5d7bc0a93c2190febaa6dab9e8f2d
en-Barry <en_barry@icloud.com>
2021-04-15 19:05:00 +0900

    Add: jQuery,Bootstrapを読み込むための記述を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require('search.js')
+require('jquery')
+require('popper.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
