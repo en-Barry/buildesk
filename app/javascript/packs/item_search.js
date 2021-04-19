d21c1219db2dc664e9f15a3ba9495c25d78a5de7
en-Barry <en_barry@icloud.com>
2021-04-20 01:37:50 +0900

    Add: アイテム情報添付時に使われるjQueryファイルを追加

--- app/javascript/packs/item_search.js
+++ app/javascript/packs/item_search.js
@@ -1,13 +1,16 @@
 $(function() {
-  $(document).on('click', ".item-search-show", function() {
+  $(document).on('click', "#item-search-show", function() {
     $('#item-search-modal').fadeIn('fast');
+    $('.modal').show();
   });
   
   $(document).on('click', "#close-modal", function() {
     $('#item-search-modal').fadeOut('fast');
+    $('#item_list').empty();
   });
 
-  $(document).on('click', '.item-search-modal-wrapper', function() {
-    $('#item-search-modal').fadeOut('fast');
-  });
+  // $(document).on('click', '.item-search-modal-wrapper', function() {
+  //   $('#item-search-modal').fadeOut('fast');
+  // });
+
 });
