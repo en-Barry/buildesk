5027c1684dd16855aa7bf60b7efc9a26f92bfbce
en-Barry <en_barry@icloud.com>
2021-04-26 17:08:19 +0900

    Add: 楽天apiで取得した商品をjson形式で一覧表示するための記述に修正

--- app/javascript/packs/item_search.js
+++ app/javascript/packs/item_search.js
@@ -1,12 +1,35 @@
 $(function() {
-  $(document).on('click', "#item-search-show", function() {
-    $('#item-search-modal').fadeIn('fast');
+  $(document).on('click', '#item-search-show', function() {
+    $('#item-search-modal').show();
     $('.modal').show();
   });
-  
-  $(document).on('click', "#close-modal", function() {
-    $('#item-search-modal').fadeOut('fast');
+
+  $(document).on('click', '#js-search-button', function(e) {
+    e.preventDefault();
+    var keyword = $('#js-search-keyword').val();
+    $.ajax({
+      url: '/search',
+      type: "GET",　//getでsearchアクションにアクセス
+      dataType: "json",
+      async: true,
+      data: { keyword: keyword },　//検索パラメーターの指定(:title)
+    }).done(function (data) {
+      $("#item_list").css("display", "");
+      $("#item_list").html(data.contents); //render json:{contents: contents}の内容表示
+    });
+    $('#js-search-keyword').val('');
+  });
+
+  $(document).on('click', '#close-modal', function() {
+    $('#item-search-modal').hide();
     $('#item_list').empty();
+  });
+
+  $(this).on('click', '.item', function() {
+    $('#item_list').hide();
+    $('#item-search-box').hide();
+    $('#item_detail').show();
+    debugger
   });
 
   // $(document).on('click', '.item-search-modal-wrapper', function() {
