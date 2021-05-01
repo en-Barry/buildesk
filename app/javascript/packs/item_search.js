a7f323a281b322bffd1c0b44e12aa664867665df
en-Barry <en_barry@icloud.com>
2021-05-01 19:30:27 +0900

    Add: 楽天APIで検索し一覧表示、詳細表示まで実装

--- app/javascript/packs/item_search.js
+++ app/javascript/packs/item_search.js
@@ -9,27 +9,32 @@ $(function() {
     var keyword = $('#js-search-keyword').val();
     $.ajax({
       url: '/search',
-      type: "GET",　//getでsearchアクションにアクセス
+      type: "GET",
       dataType: "json",
       async: true,
-      data: { keyword: keyword },　//検索パラメーターの指定(:title)
+      data: { keyword: keyword },　//検索パラメーターの指定(:keyword)
     }).done(function (data) {
-      $("#item_list").css("display", "");
-      $("#item_list").html(data.contents); //render json:{contents: contents}の内容表示
+      var items = data.contents
+      $("#item_list").html('');
+      $.each(items, function(index, val) {
+        $("#item_list").append(`<div class="search-item item">` + `<img src=` + `"${val.image}" width="150" height="150">` + `<p></p>` + val.name + `<p></p>` + `¥` + val.price + `</div>`);
+      });
+      //$("#item_list").css("display", "");
+      //$("#item_list").html(data.contents); //render json:{contents: contents}の内容表示
     });
     $('#js-search-keyword').val('');
+  });
+
+  $(this).on('click', '.item', function() {
+    var item = $(this)
+    $('#item_list').hide();
+    $('#item-search-box').hide();
+    $('#item_detail').append(item);
   });
 
   $(document).on('click', '#close-modal', function() {
     $('#item-search-modal').hide();
     $('#item_list').empty();
-  });
-
-  $(this).on('click', '.item', function() {
-    $('#item_list').hide();
-    $('#item-search-box').hide();
-    $('#item_detail').show();
-    debugger
   });
 
   // $(document).on('click', '.item-search-modal-wrapper', function() {
