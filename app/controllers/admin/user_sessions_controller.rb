d1d43c9f0e0705b802884accb879f12c7346dd1d
en-Barry <en_barry@icloud.com>
2021-05-08 06:22:15 +0900

    Fix: 管理者用ログイン、ログアウト機能のアクションの記述を修正

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,12 +1,23 @@
 class Admin::UserSessionsController < Admin::BaseController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
 
   def create
     @user = user.login(params[:email], params[:password])
 
-    if @user == 
+    if @user.admin?
+      redirect_back_or_to admin_root_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
   end
 
   def destroy
-
+    logout
+    redirect_to admin_root_path, success: t('.success')
   end
 end
