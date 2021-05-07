c81cb9de6ad8d662ed3e2c530aa32548526b31b8
en-Barry <en_barry@icloud.com>
2021-05-08 04:47:01 +0900

    Add: 管理者ユーザーログイン用のアクションを追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,2 +1,12 @@
 class Admin::UserSessionsController < Admin::BaseController
+
+  def create
+    @user = user.login(params[:email], params[:password])
+
+    if @user == 
+  end
+
+  def destroy
+
+  end
 end
