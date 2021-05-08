7884c6b15c37118076ef7107e243882adc288f6b
en-Barry <en_barry@icloud.com>
2021-05-08 09:01:05 +0900

    Add: ログインしているユーザーが権限を持っていない場合、トップページに遷移するメソッドを追加

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,10 @@
 class Admin::BaseController < ApplicationController
+  before_action :check_admin
   layout 'admin/layouts/application'
+
+  private
+
+  def check_admin
+    redirect_to root_path, warning: t('defaults.message.not_authorized') unless current_user.admin?
+  end
 end
