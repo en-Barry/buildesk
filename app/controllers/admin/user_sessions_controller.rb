a988a86e9b54bbf38197c13e315559147395a9e4
en-Barry <en_barry@icloud.com>
2021-05-08 09:02:33 +0900

    Add: ログイン前に権限者かどうかをチェックしないための設定を追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UserSessionsController < Admin::BaseController
   skip_before_action :require_login, only: %i[new create]
+  skip_before_action :check_admin, only: %i[new create]
   layout 'layouts/admin_login'
 
   def new
