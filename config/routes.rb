9107b6c7562132413fdf4fad732b4d72df3566ed
en-Barry <en_barry@icloud.com>
2021-05-08 09:04:21 +0900

    Add: 管理者用画面のトップページへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    root 'dashboards#index'
   end
   
 end
