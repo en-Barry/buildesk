d98d200537cf165ee3679402f940ef2314548f65
en-Barry <en_barry@icloud.com>
2021-05-13 12:35:16 +0900

    Add: 管理者側でユーザーを編集・削除するためのアクションを追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,34 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      render :edit, danger: t('defaults.message.not_updated', item: User.model_name.human)
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_root_path(@user), success: t('defaults.message.delete', item: User.model_name.human)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :password, :password_confirmation, :image, :image_cache)
+  end
+end
