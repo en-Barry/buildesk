5293c20be6e9cf0f5f842f9f09bdfe54cbd53946
en-Barry <en_barry@icloud.com>
2021-05-14 20:19:35 +0900

    Add: URLをハイパーテキストに変換するためのメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  require "uri"
+
   def default_meta_tags
     {
       site: 'Buildesk（ビルデスク）',
@@ -10,6 +12,15 @@ module ApplicationHelper
         { href: image_url('buildesk_favicon.png') }
       ]
     }
+  end
+
+  def text_url_to_link(text)
+    URI.extract(text, ["http", "https"]).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    text
   end
 
   private
