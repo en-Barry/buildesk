405718aafc525e265db93cc8f6374fa6476cb298
en-Barry <en_barry@icloud.com>
2021-05-26 01:20:40 +0900

    Add: navbar用のjsを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require('bootstrap')
 require('../packs/item_search')
 require('../packs/user_edit')
 require('../packs/post_edit')
+require('../packs/bulma')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
