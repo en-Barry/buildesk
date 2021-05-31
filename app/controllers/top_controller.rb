785e78ddd01633861034e65dce9c307e5d87f634
en-Barry <en_barry@icloud.com>
2021-05-31 19:59:32 +0900

    Fix: トップページに表示する新着の投稿を4つに修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@ class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
-    @posts = Post.all.includes(:user, :post_images, :categories).order(created_at: :desc).limit(3)
+    @posts = Post.all.includes(:user, :post_images, :categories).order(created_at: :desc).limit(4)
   end
 end
