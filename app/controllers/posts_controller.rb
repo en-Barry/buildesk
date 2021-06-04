0b847bc8d505d578671bc776d9588dff100b536c
en-Barry <en_barry@icloud.com>
2021-06-04 12:09:27 +0900

    Fix: 画像一覧表示時のN+1を解消するための修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,11 +48,11 @@ class PostsController < ApplicationController
   end
 
   def likes
-    @like_posts = current_user.like_posts.includes(:user).order(created_at: :desc)
+    @like_posts = current_user.like_posts.includes(:user, :post_images).order(created_at: :desc)
   end
 
   def bookmarks
-    @bookmark_posts = current_user.bookmark_posts.includes(:user).order(created_at: :desc)
+    @bookmark_posts = current_user.bookmark_posts.includes(:user, :post_images).order(created_at: :desc)
   end
 
   private
