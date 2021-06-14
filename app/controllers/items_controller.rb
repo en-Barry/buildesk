4e91a17f3a8655f2ba176f38e15c045a135b0744
en-Barry <en_barry@icloud.com>
2021-06-14 21:38:10 +0900

    Fix: Lint

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   skip_before_action :require_login, only: %i[show]
-  
+
   def show
     @item = Item.find(params[:id])
     @posts = @item.posts.includes(:user, :post_images, :categories).order(created_at: :desc)
