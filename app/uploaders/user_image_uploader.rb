e3afc14bfd2bd9dc144d75b363f71dcb8cb602b1
en-Barry <en_barry@icloud.com>
2021-06-28 00:11:59 +0900

    Fix: Lint

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -19,7 +19,7 @@ class UserImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/images/user_logo_orange.png"
+    '/images/user_logo_orange.png'
   end
 
   # Process files as they are uploaded:
