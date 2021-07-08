690e8a4dff3a7f361e515eb8c439485e9a4d0885
en-Barry <en_barry@icloud.com>
2021-07-09 06:47:20 +0900

    Fix: 本番環境でエラーの原因と遅延の原因になる記述を修正

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -3,8 +3,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require('jquery')
-require('bootstrap')
+// require('jquery')
+// require('bootstrap')
 require("admin-lte")
 
 require('../packs/user_edit.js')
