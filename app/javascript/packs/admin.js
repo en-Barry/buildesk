ed5635526633ead0ea3ca215ffa336d6c0e78771
en-Barry <en_barry@icloud.com>
2021-07-09 08:31:09 +0900

    Fix: 不必要な記述の消去とレイアウト崩れを修正

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -3,8 +3,4 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// require('jquery')
-// require('bootstrap')
-require("admin-lte")
-
 require('../packs/user_edit.js')
