bceecc830e9ca68a5a2abbacc41ec0d9cccfe1f7
en-Barry <en_barry@icloud.com>
2021-07-09 20:03:36 +0900

    Add: 画像ファイルをawsのCloudFront-S3経由でやりとりするための設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # 静的ファイルをfrontcloud経由で表示
+  config.asset_host = 'https://image.buildesk.app'
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
