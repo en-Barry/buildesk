c4bffa70335f0138e494613df9ac46b8ff5482d0
en-Barry <en_barry@icloud.com>
2021-07-10 20:25:46 +0900

    Fix: s3へのアップロードを本番環境のみ適用する設定を記述

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,17 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'ap-northeast-1'
-  }
-  config.fog_directory = 'buildesk-bucket'
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  config.asset_host = 'https://image.buildesk.app'
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory = 'buildesk-bucket'
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.asset_host = 'https://image.buildesk.app'
+  end
 end
