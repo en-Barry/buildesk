b0a1af29ba5728cbefb472007c37f8cd0a7fbd9f
en-Barry <en_barry@icloud.com>
2021-07-21 16:56:27 +0900

    Fix: twitterログインの際にメアドのユニーク制約がかかっていたので削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 } # Twitterに準拠
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }, on: :create
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, on: :create
+  validates :email, uniqueness: { case_sensitive: true }, unless: -> { email.blank? }
   validates :password, length: { in: 8..30 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
--- db/migrate/20210717124428_change_column_null_true_for_email_on_users.rb
+++ db/migrate/20210717124428_change_column_null_true_for_email_on_users.rb
@@ -1,6 +1,7 @@
 class ChangeColumnNullTrueForEmailOnUsers < ActiveRecord::Migration[6.0]
   def up
     change_column :users, :email, :string, null: false, default: ''
+    remove_index :users, :email
   end
 
   def down
