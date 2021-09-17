c656511bc104b25aa896dd384f26aa0fa71c56c2
en-Barry <en_barry@icloud.com>
2021-09-17 22:58:32 +0900

    Add: いいねしたときの挙動をAjax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
   end
 
   def destroy
-    post = Post.find(params[:id])
-    current_user.unlike(post)
-    redirect_to post_path(post)
+    @post = Post.find(params[:id])
+    current_user.unlike(@post)
   end
 end
