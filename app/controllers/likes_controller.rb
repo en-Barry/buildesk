c87670bda1632c28a9910971f835768086b5dcb4
en-Barry <en_barry@icloud.com>
2021-09-17 23:29:56 +0900

    Add: いいねのカウントをAjax処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,12 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
+    @like_count = @post.likes.count
   end
 
   def destroy
     @post = Post.find(params[:id])
     current_user.unlike(@post)
+    @like_count = @post.likes.count
   end
 end
