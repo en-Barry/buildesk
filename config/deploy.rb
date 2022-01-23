6a47fa77700f9e51ea017f9300e7f56e1aa4bbf6
en-Barry <en_barry@icloud.com>
2022-01-24 01:39:14 +0900

    Fix: 指定されたブランチがデプロイされる設定に修正
    指定方法が間違っていたため

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, "git@github.com:en-Barry/buildesk.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/app/buildesk"
 
-set :branch, 'sakura' || 'main'
+set :branch, ENV['BRANCH'] || "main"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
